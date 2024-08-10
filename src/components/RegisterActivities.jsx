21814439858c107e836e7733277a442105ef5222
Cassia <162936759+cassianebel@users.noreply.github.com>
2024-08-10 18:54:45 -0500

    update checkboxes to use state

--- src/components/RegisterActivities.jsx
+++ src/components/RegisterActivities.jsx
@@ -1,201 +1,121 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 const RegisterActivities = ({ formData, setFormData }) => {
-  const updateTotalCost = () => {
-    let totalCost = 0;
-    const checkboxes = document.querySelectorAll("#activities-box input");
-    checkboxes.forEach((checkbox) => {
-      if (checkbox.checked) {
-        totalCost += parseInt(checkbox.dataset.cost);
-      }
+  const [total, setTotal] = useState(0);
+  const [activities, setActivities] = useState([
+    {
+      name: "main",
+      time: "",
+      cost: 200,
+      title: "Main Conference",
+      checked: false,
+      disabled: false,
+    },
+    {
+      name: "frameworks",
+      time: "Tuesday 9am-12pm",
+      cost: 100,
+      title: "JavaScript Frameworks Workshop",
+      checked: false,
+      disabled: false,
+    },
+    {
+      name: "libraries",
+      time: "Tuesday 1pm-4pm",
+      cost: 100,
+      title: "JavaScript Libraries Workshop",
+      checked: false,
+      disabled: false,
+    },
+    {
+      name: "express",
+      time: "Tuesday 9am-12pm",
+      cost: 100,
+      title: "Express Workshop",
+      checked: false,
+      disabled: false,
+    },
+    {
+      name: "node",
+      time: "Tuesday 1pm-4pm",
+      cost: 100,
+      title: "Node.js Workshop",
+      checked: false,
+      disabled: false,
+    },
+    {
+      name: "buildTools",
+      time: "Wednesday 9am-12pm",
+      cost: 100,
+      title: "Build tools Workshop",
+      checked: false,
+      disabled: false,
+    },
+    {
+      name: "npm",
+      time: "Wednesday 1pm-4pm",
+      cost: 100,
+      title: "npm Workshop",
+      checked: false,
+      disabled: false,
+    },
+  ]);
+
+  const handleChange = (index) => {
+    const updatedActivities = [...activities];
+    updatedActivities[index].checked = !updatedActivities[index].checked;
+    setActivities(updatedActivities);
+    setFormData({
+      ...formData,
+      [updatedActivities[index].name]: updatedActivities[index].checked,
     });
-    document.getElementById("activities-cost").textContent =
-      `Total: $${totalCost}`;
+
+    const newTotal = updatedActivities.reduce(
+      (sum, activity) => (activity.checked ? sum + activity.cost : sum),
+      0
+    );
+    setTotal(newTotal);
   };
 
   return (
-    <fieldset
-      id="activities"
-      className="activities"
-      onChange={() => updateTotalCost()}
-    >
+    <fieldset id="activities" className="activities">
       <legend>Register for Activities</legend>
 
       <div id="activities-box" className="activities-box error-border">
-        <label className="custom-checkbox">
-          <i className="fa-solid fa-check"></i>
-          <input
-            type="checkbox"
-            name="all"
-            data-cost="200"
-            onChange={(e) =>
-              setFormData({ ...formData, main: e.target.checked })
-            }
-            checked={formData.main}
-          />
-          <span className="checkbox-mark"></span>
-          <div className="label">
-            <div className="activity-title">
-              <span>Main Conference</span>
+        {activities.map((activity, index) => (
+          <label
+            className="custom-checkbox"
+            key={activity.name}
+            htmlFor={activity.name} // Update this to match the input id
+          >
+            <i className="fa-solid fa-check"></i>
+            <input
+              id={activity.name} // Add an id that matches the label's htmlFor
+              type="checkbox"
+              name={activity.name}
+              data-day-and-time={activity.time}
+              data-cost={activity.cost}
+              checked={activity.checked}
+              disabled={activity.disabled}
+              onChange={() => handleChange(index)}
+            />
+            <span className="checkbox-mark"></span>
+            <div className="label">
+              <div className="activity-title">
+                <span>{activity.title}</span>
+                <br />
+                <span>{activity.time}</span>
+              </div>
+              <div className="activity-time-cost">
+                <span className="activity-cost">${activity.cost}</span>
+              </div>
             </div>
-            <div className="activity-time-cost">
-              <span className="activity-cost">$200</span>
-            </div>
-          </div>
-        </label>
-
-        <label className="custom-checkbox">
-          <i className="fa-solid fa-check"></i>
-          <input
-            type="checkbox"
-            name="js-libs"
-            data-day-and-time="Tuesday 9am-12pm"
-            data-cost="100"
-            onChange={(e) =>
-              setFormData({ ...formData, libraries: e.target.checked })
-            }
-            checked={formData.libraries}
-          />
-          <span className="checkbox-mark"></span>
-          <div className="label">
-            <div className="activity-title">
-              <span>JavaScript Libraries Workshop</span>
-              <br />
-              <span>Tuesday 9am-12pm</span>
-            </div>
-            <div className="activity-time-cost">
-              <span className="activity-cost">$100</span>
-            </div>
-          </div>
-        </label>
-
-        <label className="custom-checkbox">
-          <i className="fa-solid fa-check"></i>
-          <input
-            type="checkbox"
-            name="node"
-            data-day-and-time="Tuesday 1pm-4pm"
-            data-cost="100"
-            onChange={(e) =>
-              setFormData({ ...formData, node: e.target.checked })
-            }
-            checked={formData.node}
-          />
-          <span className="checkbox-mark"></span>
-          <div className="label">
-            <div className="activity-title">
-              <span>Node.js Workshop</span>
-              <br />
-              <span>Tuesday 1pm-4pm</span>
-            </div>
-            <div className="activity-time-cost">
-              <span className="activity-cost">$100</span>
-            </div>
-          </div>
-        </label>
-
-        <label className="custom-checkbox">
-          <i className="fa-solid fa-check"></i>
-          <input
-            type="checkbox"
-            name="js-frameworks"
-            data-day-and-time="Tuesday 9am-12pm"
-            data-cost="100"
-            onChange={(e) =>
-              setFormData({ ...formData, frameworks: e.target.checked })
-            }
-            checked={formData.frameworks}
-          />
-          <span className="checkbox-mark"></span>
-          <div className="label">
-            <div className="activity-title">
-              <span>JavaScript Frameworks Workshop</span>
-              <br />
-              <span>Tuesday 9am-12pm</span>
-            </div>
-            <div className="activity-time-cost">
-              <span className="activity-cost">$100</span>
-            </div>
-          </div>
-        </label>
-
-        <label className="custom-checkbox">
-          <i className="fa-solid fa-check"></i>
-          <input
-            type="checkbox"
-            name="build-tools"
-            data-day-and-time="Tuesday 1pm-4pm"
-            data-cost="100"
-            onChange={(e) =>
-              setFormData({ ...formData, buildTools: e.target.checked })
-            }
-            checked={formData.buildTools}
-          />
-          <span className="checkbox-mark"></span>
-          <div className="label">
-            <div className="activity-title">
-              <span>Build tools Workshop</span>
-              <br />
-              <span>Tuesday 1pm-4pm</span>
-            </div>
-            <div className="activity-time-cost">
-              <span className="activity-cost">$100</span>
-            </div>
-          </div>
-        </label>
-
-        <label className="custom-checkbox">
-          <i className="fa-solid fa-check"></i>
-          <input
-            type="checkbox"
-            name="npm"
-            data-day-and-time="Wednesday 9am-12pm"
-            data-cost="100"
-            onChange={(e) =>
-              setFormData({ ...formData, npm: e.target.checked })
-            }
-            checked={formData.npm}
-          />
-          <span className="checkbox-mark"></span>
-          <div className="label">
-            <div className="activity-title">
-              <span>npm Workshop</span>
-              <br />
-              <span>Wednesday 9am-12pm</span>
-            </div>
-            <div className="activity-time-cost">
-              <span className="activity-cost">$100</span>
-            </div>
-          </div>
-        </label>
-
-        <label className="custom-checkbox">
-          <i className="fa-solid fa-check"></i>
-          <input
-            type="checkbox"
-            name="express"
-            data-day-and-time="Wednesday 1pm-4pm"
-            data-cost="100"
-            onChange={(e) =>
-              setFormData({ ...formData, express: e.target.checked })
-            }
-            checked={formData.express}
-          />
-          <span className="checkbox-mark"></span>
-          <div className="label">
-            <div className="activity-title">
-              <span>Express Workshop</span>
-              <br />
-              <span>Wednesday 1pm-4pm</span>
-            </div>
-            <div className="activity-time-cost">
-              <span className="activity-cost">$100</span>
-            </div>
-          </div>
-        </label>
+          </label>
+        ))}
       </div>
       <p id="activities-cost" className="activities-cost">
-        Total: $0
+        Total: ${total}
       </p>
       <p id="activities-hint" className="activities-hint hint">
         Choose at least one activity
